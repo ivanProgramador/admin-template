f7b3186249a383ab5e1ddb9e8e5393d87edc3666
Ivan <ivan-guita@hotmail.com>
2024-11-24 17:36:56 -0300

    icines laterais adicionados

---
+++ src/components/template/MenuItem.tsx
@@ -0,0 +1,18 @@
+
+interface MenuItemProps{
+    url: string
+    texto:string
+    icone:any
+}
+
+
+export default function MenuItem(props:MenuItemProps){
+    return(
+        <li className={``}>
+
+            {props.icone}
+            
+
+        </li>
+    )
+}
